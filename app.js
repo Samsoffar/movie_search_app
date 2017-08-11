215ece31e6ddf0a89a99112dd7d2ed0811657c91
Sam Saffar <samsoffar@gmail.com>
2017-08-11 00:32:10 +0000

    get a tittle

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/results", function(req,res){
     request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
         if (!error, response.statusCode == 200) {
             var parsedData = JSON.parse(body);
-            res.send(parsedData["Search"][0]);
+            res.send(parsedData["Search"][0]["Title"]);
         }
     });
 
