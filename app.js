c621a1f2e59d20a268e19f5a6d0a5934b14efd05
Sam Saffar <samsoffar@gmail.com>
2017-08-12 16:50:12 +0000

    add search file that refers to /

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 var request = require("request");
 app.set("view engine","ejs");
+
+app.get("/",function(req, res) {
+    res.render("search");
+});
+
 app.get("/results", function(req,res){
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
+    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body) {
         if (!error, response.statusCode == 200) {
             var parsedData = JSON.parse(body);
             res.render("results", {data: parsedData});
