4dd243686dd7ed70d62fa5f59971845cb4194e31
Sam Saffar <samsoffar@gmail.com>
2017-08-12 17:21:56 +0000

    show results after passing a search query

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get("/",function(req, res) {
 });
 
 app.get("/results", function(req,res){
-    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body) {
+    var query= (req.query.search);
+    var url="http://www.omdbapi.com/?s="+query+"&apikey=thewdb"
+    request(url, function(error, response, body) {
         if (!error, response.statusCode == 200) {
             var parsedData = JSON.parse(body);
             res.render("results", {data: parsedData});
