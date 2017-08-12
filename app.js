89af858fc72a5bd6401867a02113e1e9aae04a1b
Sam Saffar <samsoffar@gmail.com>
2017-08-12 15:40:23 +0000

    change the ejs file to results, and the parsed data to parsed data

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.set("view engine","ejs");
 app.get("/results", function(req,res){
     request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
         if (!error, response.statusCode == 200) {
-            var results = JSON.parse(body);
-            res.render("results");
+            var parsedData = JSON.parse(body);
+            res.render("results", {data: parsedData});
         }
     });
 
