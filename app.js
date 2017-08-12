165e1d59875f91e1e3f52399175b0ed5113414ff
Sam Saffar <samsoffar@gmail.com>
2017-08-12 15:24:51 +0000

    displaying data, working with ejs library

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-
+app.set("view engine","ejs");
 app.get("/results", function(req,res){
     request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
         if (!error, response.statusCode == 200) {
-            var parsedData = JSON.parse(body);
-            res.send(parsedData["Search"][0]["Title"]);
+            var results = JSON.parse(body);
+            res.render("results");
         }
     });
 
